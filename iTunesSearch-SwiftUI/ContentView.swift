acbf86dc46b4f92d87dbdb1bfc56d1fabe2ca217
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-10-26 13:53:33 +0300

    Initial Commit

---
+++ iTunesSearch-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTunesSearch-SwiftUI
+//
+//  Created by Gorkem on 26.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTunesSearch-SwiftUI/iTunesSearch_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearch_SwiftUIApp.swift
+//  iTunesSearch-SwiftUI
+//
+//  Created by Gorkem on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearch_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
